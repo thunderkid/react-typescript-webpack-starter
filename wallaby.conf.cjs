1dc760ea0da5c9015bc519c4ba4240b0738fa450
thunderkid <thunderkid@smalliq.com>
2023-12-25 00:08:54 -0800

    Wallaby working, but need commonjs in wallaby.conf.cjs

--- wallaby.conf.cjs
+++ wallaby.conf.cjs
@@ -2,7 +2,7 @@ module.exports = function (wallaby) {
     return {
         files: [
             'src/**/*.ts', // Adjust the path to include your source files
-            'package.json', // Include package.json for module resolution
+            //'package.json', // Include package.json for module resolution
             // Exclude test files
             { pattern: '**/*.spec.ts', ignore: true }
         ],
@@ -14,23 +14,24 @@ module.exports = function (wallaby) {
         env: {
             type: 'node',
             runner: 'node',
-            params: {
-                runner: '--experimental-vm-modules --no-warnings' // Required for ESM
-            }
+//            params: {
+//                runner: '--experimental-vm-modules --no-warnings' // Required for ESM
+//            }
         },
 
         testFramework: 'mocha',
 
         compilers: {
             '**/*.ts?(x)': wallaby.compilers.typeScript({
-                //module: 'ESNext', // Use ESNext for ESM support
-                target: 'es2021'  // Target can be adjusted as necessary
+                module: 'commonjs', // Use ESNext for ESM support
+                target: 'es2021',  // Target can be adjusted as necessary
+                baseUrl: './src',      // 
             })
         },
 
         setup: function (wallaby) {
-            const mocha = wallaby.testFramework;
-            mocha.ui('bdd');
+            // const mocha = wallaby.testFramework;
+            // mocha.ui('bdd');
         },
 
         debug: true // Optional: Enable if you need to debug Wallaby issues
